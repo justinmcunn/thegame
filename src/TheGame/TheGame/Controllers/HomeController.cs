f6cbdbecdbf9e47e6dddceb326e6b00d40b4215d
Justin Cunningham <justinmcunn@gmail.com>
2015-04-12 16:48:00 -0500

    Add solution

---
+++ src/TheGame/TheGame/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheGame.Models;
+
+namespace TheGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Lost(string name, string from)
+        {
+            var vm = new LostModel()
+            {
+                Name = name,
+                From = from
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ src/TheGame/TheGame/Models/LostModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheGame.Models
+{
+    public class LostModel
+    {
+        public string Name { get; set; }
+        public string From { get; set; }
+    }
+}
